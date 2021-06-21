0f489dcee8cbe50d9f9671f9ff8365954d5798e0
Medos777 <medaminegh50@gmail.com>
2021-06-21 06:36:56 +0100

    vaf

--- src/app/Collecte/list-collecte/list-collecte.page.ts
+++ src/app/Collecte/list-collecte/list-collecte.page.ts
@@ -1,4 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Collecte } from 'src/app/model/collecte';
+import { CollecteService } from 'src/app/service/collecte.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-list-collecte',
@@ -7,9 +13,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCollectePage implements OnInit {
 
-  constructor() { }
+  role: any;
+  user: any;
+  list : Collecte[];
+  SearchText :string;
+  constructor( private service :CollecteService,private router:Router,private tokenService: TokenStorageService,
+    
+    public fb: FormBuilder,
+    private datePipe : DatePipe) { }
 
   ngOnInit() {
+    if(this.tokenService.getToken())
+    {
+      this.role=this.tokenService.getUser().role;
+      this.user=this.tokenService.getUser();
+    }
+    console.log(this.role);
+    this.refreshListe();
+    
   }
+refreshListe(){
+  if(this.role=="facteur"){
+
+ 
+  this.service.getAll().subscribe(
+    response =>{this.list = response;
+      console.log(response);
+    }
+   );
+  }
+  else if(this.role=="client"){
+    this.service.getDataByIdClient(this.user.id).subscribe(
+      response => { this.list = response; }
+    );
+  }
+
+}
+
+
+  onDelete(id: number) {
+   
+    if (window.confirm('Are sure you want to delete this ?')) {
+      this.service.deleteData(id)
+        .subscribe(
+          data => {
+            console.log(data);
+            this.refreshListe();
+          },
+          error => console.log(error));
+    }
+  }
+  newCollecte()
+  {
+    this.router.navigate(['addcollecte']);
+  }
+  searchByLib(){
+    if(this.SearchText=='')
+    this.refreshListe();
+    else{
+      if (this.role=="agent")
+      {
+      this.service.getDataByLib(this.SearchText).subscribe(
+        response =>{this.list = response;}
+       );
+      }
+       else 
+       {
+        this.service.getDataByLibandClient(this.SearchText,this.user.id).subscribe(
+          response =>{this.list = response;}
+         );
+
+       }
+    }
+
+    
+  }
+onSelect(item :Collecte){
+  this.service.formData = this.fb.group(Object.assign({},item));
+  this.service.choixmenu =2
+  this.router.navigate(['/addcollectes']);
+}
+transformDate(date){
+  return this.datePipe.transform(date, 'yyyy-MM-dd');
+}
+
 
 }
